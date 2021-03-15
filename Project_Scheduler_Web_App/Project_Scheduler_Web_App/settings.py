d1c45925717d2b86dcde5b0d55fc2cdaaa61c89f
jjbyrne1 <46851300+jjbyrne1@users.noreply.github.com>
2021-03-15 18:11:39 -0500

    Update settings.py
    removed secret key for django

--- Project_Scheduler_Web_App/Project_Scheduler_Web_App/settings.py
+++ Project_Scheduler_Web_App/Project_Scheduler_Web_App/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2z4nkpc-@^d_0s9f_y@_#t==nse43%kp!71i3zpg5z+a#-mv_x'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
