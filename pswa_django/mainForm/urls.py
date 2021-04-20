3cb23c116d24b0ebabf351b486ac2f93d9eecd60
jjbyrne1 <jjbyrne@ksu.edu>
2021-04-20 14:20:12 -0500

    add url page

--- pswa_django/mainForm/urls.py
+++ pswa_django/mainForm/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import showtable, my_form, my_form2
+from .views import showtable
 
 urlpatterns = [
     path('', showtable, name='form1'),
-    path('', my_form, name='form'),
-    path('', my_form2, name='form1')
 ]
