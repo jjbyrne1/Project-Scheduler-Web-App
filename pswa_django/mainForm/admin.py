edf366703d008a40b9803bd3a72c94c938f11bf8
jjbyrne1 <jjbyrne@ksu.edu>
2021-04-20 14:19:52 -0500

    add admin page

--- pswa_django/mainForm/admin.py
+++ pswa_django/mainForm/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, PresentationLog, Advisor#, TeamInformation
 
 # Register your models here.
+admin.site.register(User)
+#admin.site.register(TeamInformation)
+admin.site.register(Advisor)
+admin.site.register(PresentationLog)
