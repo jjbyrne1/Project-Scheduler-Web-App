be5fe2869bbc6dafc1cb57176797d2ffb8fd4e22
jjbyrne1 <jjbyrne@ksu.edu>
2021-04-27 18:45:06 -0500

    cleaned up views.py in MainForm and modified the object lists to be sorted in a specified order

--- pswa_django/mainForm/views.py
+++ pswa_django/mainForm/views.py
@@ -1,25 +1,20 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django import forms
-from .forms import MyForm
 from .models import User, PresentationLog, Advisor, TeamInformation
 
 
 def showtable(request):
     if request.method == "POST":
-        form = MyForm.MyForm()
         return render(request, "cv-form.html")
         #if form.is_valid:
         #    form.save()
         #return render(request, 'tableView.html', {'tableForm': form})
     else:
-        form = MyForm.MyForm()
-        advisor_list = Advisor.objects.all()
-        user_list = User.objects.all()
-        #user = user_list.get(TeamId__user=1)
-        teaminfo_list = TeamInformation.objects.all()
-        presentationlog_list = PresentationLog.objects.all()
-        return render(request, 'tableView.html', {'tableForm': form, 'advisor_list': advisor_list, 'user_list': user_list,
+        advisor_list = Advisor.objects.order_by('Name')
+        user_list = User.objects.order_by('FullName')
+        teaminfo_list = TeamInformation.objects.order_by('TeamId')
+        presentationlog_list = PresentationLog.objects.order_by('TeamId')
+        return render(request, 'tableView.html', {'advisor_list': advisor_list, 'user_list': user_list,
                                                   'teaminfo_list': teaminfo_list,
                                                   'presentationlog_list': presentationlog_list})
 
@@ -27,23 +22,3 @@ def showtable(request):
 class DateForm(forms.Form):
     #presentationinfo = PresentationLog.objects.all()
     date = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'])
-
-
-def EditTeamInformation(request):
-    advisor_list = Advisor.objects.all()
-    user_list = User.objects.all()
-    # user = user_list.get(TeamId__user=1)
-    teaminfo_list = TeamInformation.objects.all()
-    presentationlog_list = PresentationLog.objects.all()
-    if request.method == "POST":
-        form = MyForm.MyForm
-        return render(request, 'tableView.html',
-                      {'tableForm': form, 'advisor_list': advisor_list, 'user_list': user_list,
-                       'teaminfo_list': teaminfo_list,
-                       'presentationlog_list': presentationlog_list})
-    else:
-        form = MyForm.MyForm()
-        return render(request, "cv-form.html")
-        # if form.is_valid:
-        #    form.save()
-        # return render(request, 'tableView.html', {'tableForm': form})
