293e113cdb6de47621e51b6f272055c104beb663
jjbyrne1 <jjbyrne@ksu.edu>
2021-04-27 18:42:38 -0500

    cleaned up __init__.py in models

--- pswa_django/mainForm/models/__init__.py
+++ pswa_django/mainForm/models/__init__.py
@@ -1,5 +1,5 @@
 from.model_Advisor import Advisor
 from.model_PresentationLog import PresentationLog
-from.model_User import User
-#from .model_Team import Team
 from.model_TeamInformation import TeamInformation
+from.model_User import User
+
