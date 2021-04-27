23830587912e4df42d47a85b9168dccb0e5da3ad
jjbyrne1 <jjbyrne@ksu.edu>
2021-04-27 18:44:00 -0500

    cleaned up urls.py in MainForm

--- pswa_django/mainForm/urls.py
+++ pswa_django/mainForm/urls.py
@@ -4,7 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.showtable, name='tableForm'),
     path('table/', views.showtable, name='tableForm'),
-    path('info/', views.EditTeamInformation, name='infoForm')
-    #path('mainform/forms/MyForm', views.EditTeamInformation, name="infoForm"),
-    #path('', views.EditTeamInformation, name='infoForm')
 ]
