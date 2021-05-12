48f653c1f275764ab829936277f288c23d6e1429
jjbyrne1 <jjbyrne@ksu.edu>
2021-05-11 21:56:32 -0500

    added static media file that holds final project advertisement files

--- pswa_django/pswa_django/urls.py
+++ pswa_django/pswa_django/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("mainForm.urls")),
 ]
+
+# Credit to https://stackoverflow.com/questions/5871730/how-to-upload-a-file-in-django
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
+
+
